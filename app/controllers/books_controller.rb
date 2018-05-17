d3dfe512289751af0905a157bfbf07a003db8390
Matt Holloway <hollowmatt@gmail.com>
2018-05-17 09:09:50 -0400

    Working on wildcard

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,28 @@
 class BooksController < ApplicationController
 	def index
-		response = Book.__elasticsearch__.search(
-			query: {
-				multi_match: {
-					query: params[:query],
-					fields: ['name', 'author.first_name', 'author.last_name', 'isbn']
+		if (defined? params[:query])
+			response = Book.__elasticsearch__.search(
+				query: {
+					multi_match: {
+						query: params[:query],
+						fields: ['name', 'author.first_name', 'author.last_name', 'isbn']
+					}
 				}
+			).results
+			
+			render json: {
+				results: response.results,
+				total: response.total
 			}
-		).results
-
-		render json: {
-			results: response.results,
-			total: response.total
-		}
+		else
+			response = Book.__elasticsearch__.search(
+				query: {
+					wildcard: { 
+						"name": "*"
+					}
+				}
+			).results
+		puts results
+		end
 	end
 end
